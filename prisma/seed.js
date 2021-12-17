39220d0aae0971b710fd4bd29881cbfeb1fb99fd
Felipe Villegas <felavid@gmail.com>
2021-12-16 22:27:46 -0500

    Seed Prisma.

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,9 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+const { PrismaClient } = require('@prisma/client')
 const db = new PrismaClient();
 
 async function seed() {
   await Promise.all(getPosts().map(post => db.post.create({ data: post })));
 }
+
+seed()
 
 function getPosts() {
   return [
